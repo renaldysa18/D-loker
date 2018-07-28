6f66983dd2efa7efb3fcee69d22d25deca00d4da
renaldy sabdo <renaldysab18@gmail.com>
2018-07-29 01:35:17 +0700

    bottom, drawer, fragment

--- app/src/main/java/com/example/renaldysabdojatip/dloker/MainActivity.java
+++ app/src/main/java/com/example/renaldysabdojatip/dloker/MainActivity.java
@@ -1,13 +1,136 @@
 package com.example.renaldysabdojatip.dloker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+import java.sql.Time;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomNavigationView;
+
+    private FrameLayout mainFrame;
+
+    private ProfileFragment pf;
+    private SearchFragment sf;
+    private BookmarkFragment bf;
+    private RiwayatFragment rf;
+    private TimelineFragment tf;
+    private RekomendasiFragment rekf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.main_nav);
+
+        //init fragment
+
+        pf = new ProfileFragment();
+        sf = new SearchFragment();
+        bf = new BookmarkFragment();
+        rf = new RiwayatFragment();
+        tf = new TimelineFragment();
+        rekf = new RekomendasiFragment();
+
+
+        //init timeline fragment
+        setFragment(tf);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.bottom_profile:
+                        setFragment(pf);
+                        return true;
+
+                    case R.id.bottom_rekom:
+                        setFragment(rekf);
+                        return true;
+
+                    case R.id.bottom_search:
+                        setFragment(sf);
+                        return true;
+
+                    case R.id.bottom_timeline:
+                        setFragment(tf);
+                        return true;
+
+                    default:
+                        return false;
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.drawer_bookmark) {
+            setFragment(bf);
+        } else if (id == R.id.drawer_riwayat) {
+            setFragment(rf);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void setFragment(Fragment fragment) {
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.main_frame, fragment);
+        ft.addToBackStack(null);
+        ft.commit();
+
+
     }
 }
