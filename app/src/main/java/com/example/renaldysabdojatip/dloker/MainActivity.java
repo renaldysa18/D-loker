6e34dca606770241fad61d16e5d7bd81dfc97b5e
renaldy sabdo <renaldysab18@gmail.com>
2018-07-29 22:42:32 +0700

    minus picture, sama spinner

--- app/src/main/java/com/example/renaldysabdojatip/dloker/MainActivity.java
+++ app/src/main/java/com/example/renaldysabdojatip/dloker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.renaldysabdojatip.dloker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -7,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -17,13 +19,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final String TAG = "MAIN_ACTIVITY";
     private BottomNavigationView bottomNavigationView;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mListener;
 
     private FrameLayout mainFrame;
 
@@ -56,6 +66,27 @@ public class MainActivity extends AppCompatActivity
 
         //init timeline fragment
         setFragment(tf);
+
+        //firebase
+        mAuth = FirebaseAuth.getInstance();
+
+        mListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if(user != null){
+                    Log.d(TAG, "sing in : "+user.getUid() );
+                } else {
+                    Toast.makeText(getApplicationContext(), "Keluar ", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(getApplicationContext(), Login.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+
+            }
+        };
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -117,6 +148,8 @@ public class MainActivity extends AppCompatActivity
             setFragment(bf);
         } else if (id == R.id.drawer_riwayat) {
             setFragment(rf);
+        } else if(id == R.id.drawer_signout){
+            mAuth.signOut();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -130,7 +163,27 @@ public class MainActivity extends AppCompatActivity
         ft.replace(R.id.main_frame, fragment);
         ft.addToBackStack(null);
         ft.commit();
+    }
 
+    public void setActionBarTitle(String title){
+        getSupportActionBar().setTitle(title);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mListener);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if(mListener != null){
+
+            mAuth.removeAuthStateListener(mListener);
+        }
     }
 }
